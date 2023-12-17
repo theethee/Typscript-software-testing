6bbd22eacd01db0c19ccde4ec18f653c709d1bb5
Thitisuda Mahasrithanet <thiti.m90@gmail.com>
2023-12-17 14:10:22 +0100

    fixed styling home

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,18 +1,21 @@
 import "../Home/Home.css";
+import "../../index.css";
 
 function Home() {
   return (
     <>
       <div id="home-container">
         <h1>NexGen</h1>
-        <p className="input-text">Username/emal</p>
-        <input id="field-gap" className="sign-in-field" type="text" />
-        <p className="input-text">Password</p>
-        <input className="sign-in-field" type="password" />
-        <a className="links">Forgot your password?</a>
-        <button id="sign-in-button">Sing in</button>
-        {/* ska vara en länk här */}
-        <a className="links">No account? Register here</a>
+        <form id="home-form">
+          <label className="input-text">Username/emal</label>
+          <input id="field-gap" className="sign-in-field" type="text" />
+          <label className="input-text">Password</label>
+          <input className="sign-in-field" type="password" />
+          <a className="links">Forgot your password?</a>
+          <button id="sign-in-button">Sing in</button>
+          {/* ska vara en länk här */}
+          <a className="links">No account? Register here</a>
+        </form>
       </div>
     </>
   );
