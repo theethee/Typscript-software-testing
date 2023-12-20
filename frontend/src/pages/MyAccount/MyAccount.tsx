3734fc801487a82be66c627e5f2807b1df57d39c
Thitisuda Mahasrithanet <thiti.m90@gmail.com>
2023-12-20 15:57:51 +0100

    Adjust MyAccount

--- frontend/src/pages/MyAccount/MyAccount.tsx
+++ frontend/src/pages/MyAccount/MyAccount.tsx
@@ -6,12 +6,16 @@ import deleteIcon from "../../assets/delete-icon.png";
 import deleteAccount from "../../assets/delete-icon-red.png";
 
 function MyAccount() {
+  // 1. Ska hämta personuppgifter och visa
+  // 2. Det ska gå att edita
+  // 3. Det ska gå att ta bort kontot
   return (
     <>
       {/* Ändra till formulär! */}
       <div id="my-account-container">
         <h1>NexGen</h1>
         <h3>My account</h3>
+
         <img
           id="no-img-picked"
           src={noImg}
@@ -21,36 +25,58 @@ function MyAccount() {
           <img id="add-img" src={addImg} alt="icon to add image" />
         </div>
 
-        <div id="change-personinfo">
-          <div className="align-info">
-            <p className="p-styling">[Username]</p>
-            <div className="align-under-p">
+        <form id="edit-form">
+          <label className="style-label" htmlFor="text">
+            Username
+          </label>
+          <div className="align-input-edit-icon">
+            <input className="input-styling" type="text" />
+            <div className="align-edit-icons">
               <img className="change-icon" src={edit} alt="edit icon" />
-              <img className="change-icon" src={deleteIcon} alt="delete icon" />
             </div>
           </div>
 
-          <div className="align-info">
-            <p className="p-styling">[Password]</p>
-            <div className="align-under-p">
+          <label className="style-label" htmlFor="text">
+            First name
+          </label>
+          <div className="align-input-edit-icon">
+            <input className="input-styling" type="text" />
+            <div className="align-edit-icons">
               <img className="change-icon" src={edit} alt="edit icon" />
-              <img className="change-icon" src={deleteIcon} alt="delete icon" />
             </div>
           </div>
 
-          <div className="align-info">
-            <p className="p-styling">[Email]</p>
-            <div className="align-under-p">
-              <img
-                // id="align-email-icons"
-                className="change-icon"
-                src={edit}
-                alt="edit icon"
-              />
-              <img className="change-icon" src={deleteIcon} alt="delete icon" />
+          <label className="style-label" htmlFor="text">
+            Last name
+          </label>
+          <div className="align-input-edit-icon">
+            <input type="text" className="input-styling" />
+            <div className="align-edit-icons">
+              <img className="change-icon" src={edit} alt="edit icon" />
             </div>
           </div>
-        </div>
+
+          <label className="style-label" htmlFor="text">
+            Email
+          </label>
+          <div className="align-input-edit-icon">
+            <input className="input-styling" type="text" />
+            <div className="align-edit-icons">
+              <img className="change-icon" src={edit} alt="edit icon" />
+            </div>
+          </div>
+
+          <label className="style-label" htmlFor="text">
+            Password
+          </label>
+          <div className="align-input-edit-icon">
+            <input className="input-styling" type="text" />
+            <div className="align-edit-icons">
+              <img className="change-icon" src={edit} alt="edit icon" />
+            </div>
+          </div>
+        </form>
+
         <div id="align-delete-account-button">
           <button id="delete-account-button">
             Delete account
