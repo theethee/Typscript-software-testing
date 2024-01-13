03be2f04a0b8d4a76c248cf9fc1f6708255be0e3
Thitisuda Mahasrithanet <thiti.m90@gmail.com>
2024-01-13 15:01:22 +0100

    testBurger fixed

--- frontend/cypress/component/testBurger.cy.tsx
+++ frontend/cypress/component/testBurger.cy.tsx
@@ -3,13 +3,13 @@ import Navbar from "../../src/components/Navbar/Navbar";
 import { MemoryRouter } from "react-router-dom";
 
 describe("Testing burger menu component test", () => {
-  mount(
-    <MemoryRouter>
-      <Navbar />
-    </MemoryRouter>
-  );
-
   it("Click on burger menu and the dropdown menu shows up then close menu", () => {
+    mount(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>
+    );
+    cy.get("#burger").should("be.visible");
     cy.get("#burger").click();
     cy.get("#burger").click();
   });
